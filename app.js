bf7e332cd7bef8ae9e768bb8072cfeb9a381dbaf
Vatsalya Sharma <21bee025@nith.ac.in>
2022-10-04 11:06:29 +0530

    Fixed a bug

--- app.js
+++ app.js
@@ -146,11 +146,11 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-// let port = process.env.PORT;
-// if (port == null || port == "") {
-//   port = 3000;
-// }
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log("Server has started successfully");
 });
